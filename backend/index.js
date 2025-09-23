d826e59e8ef88c80e173d7a943d29fe4f7cee879
aaron1-z <adityakittu2773@gmail.com>
2025-09-24 03:55:09 +0530

    WIP: local changes before branch switch

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(config.mongoURI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
     console.error('MongoDB connection error:', err);
