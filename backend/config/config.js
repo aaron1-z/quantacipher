b1814279a9ea6707684cbecae642093a7bc325ce
Aditya Singh <adityakittu2773@gmail.com>
2026-01-07 12:22:37 +0530

    Add config.js file for backend configuration

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,1 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://adityakittu2773_db_user:juIcEfD7fdVvOjQb@cluster0.osr7t1b.mongodb.net/quantacipher?retryWrites=true&w=majority&appName=Cluster0',
-    secretOrKey: '936c9baa5f9b31dedf38293d3ec7b5702bfba567e9f30a644bc854c4f5f4b982cd279bf0ac7f34db34fbfc54550310d7d31762d2a53f314b7338ee6b27d18977' // Your strong secret key for JWT
-  };
-  
+.....
